20a1570632c2d4c69dcc694fabd06044afb92dbc
Suramate Chokchaisuwan <suramate.vc92@gmail.com>
2023-03-29 22:31:36 +0700

    first try

---
+++ Buyers/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from .middlewares.auth import auth_middleware
+from .views import Index, store, Signup, Login, logout, Cart, CheckOut, OrderView
+
+# URLConf
+urlpatterns = [
+    path('', Index.as_view(), name='homepage'),
+    path('store', store, name='store'),
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout, name='logout'),
+    path('cart', auth_middleware(Cart), name='cart'),
+    path('check-out', CheckOut.as_view(), name='checkout'),
+    path('orders', auth_middleware(OrderView), name='orders')
+]
