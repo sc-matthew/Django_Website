d30c903eeb3dfb5b90ed789ca751d8e603811835
Suramate Chokchaisuwan <suramate.vc92@gmail.com>
2023-04-01 20:12:29 +0700

    closed for outside business hours

--- FigureStore/settings.py
+++ FigureStore/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # "Buyers.middlewares.business_hours_check.BusinessHoursMiddleware"
 ]
 
 ROOT_URLCONF = "FigureStore.urls"
