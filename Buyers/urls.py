5fb6a49178f42cd993679070378769ec4e9b69f3
Dhawalrat Leelapratak <dhawalratlee@gmail.com>
2023-04-03 23:30:39 +0700

    fix_search

--- Buyers/urls.py
+++ Buyers/urls.py
@@ -6,7 +6,7 @@ from .views import Index, store, Signup, Login, logout, Cart, CheckOut, OrderVie
 # URLConf
 urlpatterns = [
     path("", Index.as_view(), name="homepage"),
-    path("store", store, name="store"),
+    path("store/", store, name="store"),
     path("signup", Signup.as_view(), name="signup"),
     path("login", Login.as_view(), name="login"),
     path("logout", logout, name="logout"),
