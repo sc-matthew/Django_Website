22c1f8e66f65cb1c3939c0eb6f72b15d7ce92deb
Suramate Chokchaisuwan <suramate.vc92@gmail.com>
2023-04-18 11:07:56 +0700

    product status Ready/Not Available

--- Vendors/models.py
+++ Vendors/models.py
@@ -55,6 +55,7 @@ class Products_v(models.Model):
     description = models.CharField(max_length=2500, default="", blank=True, null=True)
     image = models.ImageField(upload_to="uploads/products/")
     last_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
 
     @staticmethod
     def get_products_by_id(ids):
