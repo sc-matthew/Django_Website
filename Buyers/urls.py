589009293b61995d505f289ab65305e7b87a6afe
Dhawalrat Leelapratak <dhawalratlee@gmail.com>
2023-04-19 00:10:39 +0700

    authen

--- Buyers/urls.py
+++ Buyers/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, re_path
 from .middlewares.auth import auth_middleware
 from . import views
-from .views import Index, store, Signup, Login, logout, Cart, CheckOut, OrderView,product_details, add_to_cart, like_product, product_search
+from .views import Index, store, Signup, Login, logout, Cart, CheckOut, OrderView,product_details, add_to_cart, product_search
 
 # URLConf
 urlpatterns = [
@@ -15,7 +15,7 @@ urlpatterns = [
     path("check-out", CheckOut.as_view(), name="checkout"),
     path('store/product_details/<int:product_id>/', product_details.as_view(), name='product_details'),
     path('add-to-cart/', add_to_cart, name='add_to_cart'),
-    path('product_details/<int:product_id>/like/', like_product, name='like_product'),
+    # path('product_details/<int:product_id>/like/', like_product, name='like_product'),
     path('search/', product_search, name='product_search')
 
 ]
