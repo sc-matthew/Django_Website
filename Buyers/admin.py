b6c041c4ed4b99b92962ee971c59a633a45035d1
Dhawalrat Leelapratak <dhawalratlee@gmail.com>
2023-05-05 13:16:50 +0700

    admin finish

--- Buyers/admin.py
+++ Buyers/admin.py
@@ -1,17 +1,54 @@
 from django.contrib import admin
-from .models import  Customer, Order
+from . import models
 
 
 # Register your models here.
-class AdminProduct(admin.ModelAdmin):
-    list_display = ["name", "price", "category"]
-    list_editable = ["price"]
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display =[
+        "first_name",
+        "last_name",
+        "phone",
+        "email",
+    ]
+    list_editable = [
+        "phone",
+        "email",
+    ]
+    search_fields = ['first_name__startswith', 'last_name__startswith']
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display=[
+        "product",
+        "get_customer_first_name",
+        "get_customer_last_name",
+        "quantity",
+        "price",
+        "address",
+        "phone",
+        "date",
+        "status"
+    ]
 
+    list_editable = [
+        "quantity",
+        "price",
+        "address",
+        "phone",
+        "status",
+    ]
 
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(Order)
+    search_fields = ['product']
+
+    def get_customer_first_name(self, obj):
+        return obj.customer.first_name
+    
+    def get_customer_last_name(self, obj):
+        return obj.customer.last_name
+    
+    get_customer_first_name.short_description = 'Customer First Name'
+    get_customer_last_name.short_description = 'Customer Last Name'
--- Vendors/admin.py
+++ Vendors/admin.py
@@ -1,18 +1,63 @@
 from django.contrib import admin
-from .models import Vendors, Products_v, Category_v
+from . import models
 
-# Register your models here.
-class AdminProduct_v(admin.ModelAdmin):
-    list_display = ["name", "price", "category", "ownerid"]
-    list_editable = ["price", "ownerid"]
+@admin.register(models.Vendors)
+class VendorsAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display=[
+        "store_name",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "address",
+        "open_hour",
+        "to_hour",
+    ]
 
+    list_editable=[
+        "email",
+        "phone",
+        "open_hour",
+        "to_hour",
+    ]
+
+    search_fields = ["store_name"]
+
+    def first_name(self, obj):
+        return obj.contact_person_first_name
+    
+    def last_name(self, obj):
+        return obj.contact_person_last_name
+    
+    first_name.short_description = "First Name"
+    last_name.short_description = "Last Name"
+
+@admin.register(models.Category_v)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_per_page = 10
+    list_display=[
+        "name"
+    ]
 
+    search_fields=['name']
 
-# Register your models here.
+@admin.register(models.Products_v)
+class ProductsAdmin(admin.ModelAdmin):
+    list_per_page=10
+    list_display=[
+        "name",
+        "price",
+        "category",
+        "last_update",
+        "status",
 
-# Register your models here.
-admin.site.register(Vendors)
-admin.site.register(Products_v,AdminProduct_v)
-admin.site.register(Category_v)
+    ]
+
+    list_editable =[
+        "price",
+        "category",
+        "status"
+    ]
+
+    search_fields = ['name']
