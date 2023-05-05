c9fdc4d32752ef129010fd6f1fc96081434fbb1e
Suramate Chokchaisuwan <suramate.vc92@gmail.com>
2023-05-05 23:51:01 +0700

    add static

--- FigureStore/settings.py
+++ FigureStore/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = '/media/'
